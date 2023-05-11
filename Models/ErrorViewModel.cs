4817c4baa175486019c19554ff7b498fe5a7bfdc
Scorevictim <133180045+SRZhangA@users.noreply.github.com>
2023-05-10 18:59:02 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Antra_SEP_ASPNETCORE_MVC_Assignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
