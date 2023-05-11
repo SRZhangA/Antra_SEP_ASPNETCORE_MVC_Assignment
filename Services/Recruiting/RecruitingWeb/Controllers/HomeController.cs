e2672927944959666f1a82b122e99a6f2a948359
Scorevictim <133180045+SRZhangA@users.noreply.github.com>
2023-05-11 14:09:21 -0700

    add skeleton repository and service files

--- Services/Recruiting/RecruitingWeb/Controllers/HomeController.cs
+++ Services/Recruiting/RecruitingWeb/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace RecruitingWeb.Controllers
             return View();
         }
 
+        public IActionResult Jobs()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
